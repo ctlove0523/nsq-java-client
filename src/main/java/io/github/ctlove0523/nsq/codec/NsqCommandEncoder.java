c00e7ed48c767cb31eb2f8a230df8baf16e961a5
ctlove0523 <478309639@qq.com>
2022-04-05 00:15:48 +0800

    encode nsq command

---
+++ src/main/java/io/github/ctlove0523/nsq/codec/NsqCommandEncoder.java
@@ -0,0 +1,44 @@
+package io.github.ctlove0523.nsq.codec;
+
+import io.github.ctlove0523.nsq.cmd.NsqCommand;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NsqCommandEncoder extends MessageToMessageEncoder<NsqCommand> {
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, NsqCommand cmd, List<Object> out) throws Exception {
+        List<String> commandLineParts = new ArrayList<>();
+        commandLineParts.add(cmd.commandName().cmdName());
+        commandLineParts.addAll(cmd.commandParams());
+        String commandLine = String.join(" ", commandLineParts);
+
+        ByteBuf buf = Unpooled.buffer();
+        buf.writeBytes(commandLine.getBytes(StandardCharsets.UTF_8));
+
+        // Publish multiple messages to a topic (atomically)
+        if (cmd.commandBody().size() > 1) {
+            // write total body size and message size
+            int bodySize = 4; //4 for total messages int.
+            for (byte[] messageBody : cmd.commandBody()) {
+                bodySize += 4; //message size
+                bodySize += messageBody.length;
+            }
+            buf.writeInt(bodySize);
+            buf.writeInt(cmd.commandBody().size());
+        }
+
+        for (byte[] data : cmd.commandBody()) {
+            buf.writeInt(data.length);
+            buf.writeBytes(data);
+        }
+
+        out.add(buf);
+    }
+}
